060b4c0a7b52f5140d146321c7d7cc23bf2ee0b1
Crysknight <daniel.mc-cunt@yandex.ru>
2017-02-15 12:06:32 +0300

    removed useless code in App.js

--- src/App.js
+++ src/App.js
@@ -3,15 +3,6 @@ import './App.css';
 import AnimalsCounter from './animalsCounter/animalsCounter';
 
 class App extends Component {
-  constructor(props) {
-  	super(props);
-  	this.state = {
-  		value: 0
-  	};
-  }
-  changeValue(value) {
-  	this.setState({value});
-  }
   render() {
     return (
       <div className="App">
